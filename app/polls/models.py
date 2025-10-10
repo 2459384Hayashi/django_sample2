c92a96ec0fe0a7b29e97f5234a5cbc885a35a8ed
djangouser <kmm2459384@stu.o-hara.ac.jp>
2025-10-10 14:57:49 +0900

    commit

---
+++ app/polls/models.py
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+=======
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+>>>>>>> 7c58387c4c9a57ac9303404d7147914887963e31
+    votes = models.IntegerField(default=0)
